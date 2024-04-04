1ca03fdea5aa9d690b24b96841c03e943547de8a
ajito nelson <8997ajito@gmail.com>
2024-04-04 12:37:25 +0900

    .

---
+++ casting type data/variavel.py
@@ -0,0 +1,31 @@
+#1. **Kaste husi int iha float**:
+x = 5.5
+y = int(x)  # y sei sai 5
+   
+
+#2. **Kaste husi float iha int**:
+x = 10
+y = float(x)  # y sei sai 10.0
+   
+
+#3. **Kaste husi int ka float iha string**:
+x = "10"
+y = int(x)     # y sei sai 10
+z = float(x)   # z sei sai 10.0
+   
+
+#4. **Kaste husi string iha int ka float**:
+x = "10"
+y = int(x)     # y sei sai 10
+z = float(x)   # z sei sai 10.0
+   
+
+#5. **Kaste husi int iha string**: 
+x = 10
+y = str(x)   # y sei sai "10"
+   
+
+#6. **Kaste husi float iha string**:  
+x = 3.14
+y = str(x)   # y sei sai "3.14"
+   
